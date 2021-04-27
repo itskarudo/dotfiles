22577ee550c57c3f9eda58649d194d9876ab51af
itskarudo <itskarudo@protonmail.com>
2021-04-28 00:24:06 +0100

    finally edited rofi

---
+++ rofi/.config/rofi/applets/powermenu.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+
+dir="$HOME/.config/rofi/applets/configs"
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+cpu=$(sh ~/.config/rofi/bin/usedcpu)
+memory=$(sh ~/.config/rofi/bin/usedram)
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $HOME/.config/rofi/misc/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$HOME/.config/rofi/misc/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "UP - $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $lock)
+		if [[ -f /usr/local/bin/slock ]]; then
+			slock &
+		fi
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			bspc quit
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
---
+++ rofi/.config/rofi/launcher/launch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+theme="$HOME/.config/rofi/launcher/theme.rasi"
+
+rofi -no-lazy-grab -show drun -modi run,drun,window -theme $theme
